ce518a1f4d2ad65e459965ca1026c9d5dd0dce4c
Francois Panganiban <francois.panganiban@estiam.com>
2023-01-13 15:46:02 +0100

    -created: Square component

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Square from "./components/Square";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <Square text="A"></Square>
+        <Square text="B"></Square>
+        <Square text="C"></Square>
+      </View>
+      <View style={styles.row}>
+        <Square text="D"></Square>
+        <Square text="E"></Square>
+        <Square text="F"></Square>
+      </View>
+      <View style={styles.row}>
+        <Square text="G"></Square>
+        <Square text="H"></Square>
+        <Square text="I"></Square>
+      </View>
     </View>
   );
 }
@@ -13,8 +26,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#5762B7",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
   },
 });
